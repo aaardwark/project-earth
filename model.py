319f0205d9bb42794f0938fff2b0c30fab332c8f
aaardwark <pranav.ac1729@gmail.com>
2025-05-11 08:10:48 +0530

    latest version

--- model.py
+++ model.py
@@ -36,22 +36,16 @@ PERIOD = 365.256
 
 ##### SETUP #####
 EARTH = Sphere(ORIGIN, EARTH_RADIUS)
-EARTH_AXIS = Z_AXIS
 NORTH_POLE = Point(0,0,EARTH_RADIUS)
 
-ORBITAL_PLANE = Plane.from_coefficients( tan(radians(EARTH_AXIAL_TILT)) , 0, -1, 0)
 
-def compute_eccentric_anomaly(mean_anomaly):
-    """mean anomaly is angle representing the duration of the orbit, starting from periapsis, that has been traversed \n
-    eccentric anomaly is the angle at the centre of the orbit between periapsis and the projected point on the auxiliary circle"""
+def sun_position(mean_anomaly):
+    "mean anomaly: angle representing the duration of the orbit, starting from periapsis, that has elapsed. returns a Point"
+    # compute eccentric anomaly
     Ei = [0, 1] # arbitrary starting values; -pi< <pi
     while abs(Ei[1] - Ei[0]) > 0.0001:
         Ei.append( mean_anomaly + ECCENTRICITY*sin(Ei.pop(0)) )
-    return Ei.pop()
-
-def sun_position(days_since_periapsis):
-    "returns a Point"
-    eccentric_anomaly = compute_eccentric_anomaly(2*pi*days_since_periapsis/PERIOD)
+    eccentric_anomaly = Ei.pop()
 
     position_1 = Point(SEMIMAJOR*(cos(eccentric_anomaly) - ECCENTRICITY), SEMIMINOR*sin(eccentric_anomaly), 0)
     "in ellipse-axes frame of reference, treating xy plane as orbital plane"
@@ -65,7 +59,7 @@ def sun_position(days_since_periapsis):
     return position_3
 
 
-def location_on_earth(time, latitude, longitude_ref):
+def location_on_earth(time, latitude_radians, longitude_ref):
     """time of day as the angle of rotation completed by the earth since 0000 hrs; radians \n
     latitude; degrees \n
     the sun - a reference point to define where on earth noon/midnight are; Point"""
@@ -73,7 +67,7 @@ def location_on_earth(time, latitude, longitude_ref):
     corrected_longitude = ref_longitude - time
     longitude_vector = Vector(cos(corrected_longitude), sin(corrected_longitude), 0)
     
-    axis_point_at_lat = Point(0,0, EARTH_RADIUS * sin(radians(latitude)) )
+    axis_point_at_lat = Point(0,0, EARTH_RADIUS * sin(latitude_radians) )
     ray_through_location = Ray(axis_point_at_lat, longitude_vector)
     return ray_through_location.intersect(EARTH)
 
@@ -85,21 +79,45 @@ def compute_observed_angles(location_on_earth, position_of_sun):
     north_ray_proj = north_ray.project_onto_plane(location_tangent)
     sun_ray = Ray.from_points(location_on_earth, position_of_sun)
     sun_ray_proj = sun_ray.project_onto_plane(location_tangent)
-    # typerror if location is given as n pole
+    # typerror ray from equal points if location is given as n pole
     # have to handle case where sun is directly above location - projection will be a point
 
     bearing = north_ray_proj.vector.angle_to(sun_ray_proj.vector)
     # angle_to cannot differentiate symmetry
 
 
+def parse_timestr_24hrs(timestr_24hrs):
+    def invalid():
+        raise
+    truncated_time_str = timestr_24hrs[:4]
+    invalid() if len(truncated_time_str) < 4 else 0
+    try:
+        time_int = int(invalid)
+    except:
+        invalid()
+    invalid() if time_int < 0 or time_int > 2400 else 0
+
+    hours, minutes = divmod(time_int, 60)
+    return {'hours':hours, 'minutes':minutes}
+
+
 def get_inputs():
-    "should return time, latitude and days since periapsis as a tuple. rewrite according to how input is to be taken"
+    "should return time angle, latitude and mean anomaly as a tuple. rewrite according to how input is to be taken"
     time_inp = input('Enter the time in 24 hour format (only numbers - no spaces, dots, etc): ')
     latitude_inp = input('Enter your latitude in degrees: ')
     days_since_periapsis_inp = input('Enter number of days since periapsis: ')
+
     # process inputs and convert to appropriate format
+    try:
+        time_angle = None # TBD
+        latitude_rad = radians(float(latitude_inp))
+        mean_anomaly = (2*pi/PERIOD) * (int(days_since_periapsis_inp) % PERIOD)
+        return time_angle, latitude_rad, mean_anomaly
+    except Exception as e:
+        print(f'{e.__class__.__name__}: {e.args[0]}')
+        return get_inputs()
+        
 
-
-
+time_angular, latitude_radians, mean_anomaly = get_inputs()
 SUN = sun_position()
 LOCATION = location_on_earth()
